e2d69b2f2aa17d40a2c80328b64683c32eac4d46
MatthewAntle <42783092+MatthewAntle@users.noreply.github.com>
2020-02-13 14:49:56 -0600

    Assignment 3 index.js and Swagger Contract

---
+++ index.js
@@ -0,0 +1,32 @@
+const item = {
+  itemId: '02bbdbc7-e22e-4153-abd8-b5732a4ba6a7',
+  name: 'Basketball T-Shirt',
+  description: "Women's Basketball T-Shirt",
+  price: 32.99,
+  size: 'Medium'
+};
+
+const customer = {
+  customerId: '02bbdbc7-e22e-4153-abd8-b5732a4ba6a8',
+  firstName: 'random',
+  lastName: 'person',
+  email: 'random.person@drake.edu',
+  phoneNumber: '1234567890'
+};
+
+const cart = {
+  customerId : '02bbdbc7-e22e-4153-abd8-b5732a4ba6a8',
+  createdDate : '2-01-2020',
+  purchasedDate : '2-02-2020'  
+};
+
+const cartItems = {
+  itemId : ['02bbdbc7-e22e-4153-abd8-b5732a4ba6a7'],
+  itemName : ["Basketball T-Shirt"],
+  quantity : [3]
+};
+
+console.log(item);
+console.log(customer);
+console.log(cart);
+console.log(cartItems);
