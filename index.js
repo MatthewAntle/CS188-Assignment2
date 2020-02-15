900d0067ccfedcd968bcd76ef62f14cba43c5ac8
Dheeraj Srivatsav <dpstm3@mail.missouri.edu>
2020-02-15 10:44:59 -0600

    Update index.js
    Good job, minor suggestions
    
    `cartItems` will be a list, every cartItem will be always associated with a corresponding cartId.
    
    In database design this is known as referential constraint. 
    A child record cannot exist without the corresponding parent.

--- index.js
+++ index.js
@@ -15,16 +15,28 @@ const customer = {
 };
 
 const cart = {
+  cartId: '09bbdbc9-e22e-3514-abd8-b5732a4ba6a9',
   customerId : '02bbdbc7-e22e-4153-abd8-b5732a4ba6a8',
   createdDate : '2-01-2020',
   purchasedDate : '2-02-2020'  
 };
 
-const cartItems = {
-  itemId : ['02bbdbc7-e22e-4153-abd8-b5732a4ba6a7'],
-  itemName : ["Basketball T-Shirt"],
-  quantity : [3]
-};
+const cartItems = [{
+  cartId: '09bbdbc9-e22e-3514-abd8-b5732a4ba6a9',
+  itemId : '02bbdbc7-e22e-4153-abd8-b5732a4ba6a7',
+  itemName : "Basketball T-Shirt",
+  quantity : 3
+}, {
+  cartId: '09bbdbc9-e22e-3514-abd8-b5732a4ba6a9',
+  itemId : '03bbdbc7-e22e-4153-abd8-b5732a4ba6a5',
+  itemName : "Football T-Shirt",
+  quantity : 2
+}, {
+  cartId: '09bbdbc9-e22e-3514-abd8-b5732a4ba6a9',
+  itemId : '01bbdbc7-e22e-4153-abd8-b5732a4ba2a9',
+  itemName : "Baseball T-Shirt",
+  quantity : 1
+}];
 
 console.log(item);
 console.log(customer);
