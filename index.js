ebaea0b0a1f532e7d05278dcf07243d3ba9601d1
MatthewAntle <42783092+MatthewAntle@users.noreply.github.com>
2020-04-12 11:37:53 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -1,132 +1,28 @@
 const Hapi = require('@hapi/hapi');
-const uuid = require('uuid');
+
+const {initCustomerControllers} = require('./controllers/customer-controller');
+const {initCartControllers} = require('./controllers/cart-controller');
+const {initItemControllers} = require('./controllers/item-controller');
+const {initCartItemControllers} = require('/controllers/cart-item-controller');
 
 const init = async () => {
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
 
-	const server = Hapi.server({
-		port: 3000,
-		host: 'localhost'
-	});
+    initCustomerControllers(server);
+    initCartControllers(server);
+    initItemControllers(server);
+    initCartItemControllers(server);
 
-	const customerDave = {
-		customerId: uuid.v4(),
-		firstName: "Dave",
-		lastName: "David",
-		email: "dave.david@drake.edu",
-		phoneNumber: "123-456-7890"
-	};
-
-	const customerMike = {
-		customerId: uuid.v4(),
-		firstName: "Mike",
-		lastName: "Michael",
-		email: "mike.michael@drake.edu",
-		phoneNumber: "123-456-7891"
-	};
-	
-	const customerJake = {
-		customerId: uuid.v4(),
-		firstName: "Jake",
-		lastName: "Jacob",
-		email: "jake.jacob@drake.edu",
-		phoneNumber: "123-456-7892"
-	};
-
-	let customers = [customerDave, customerMike, customerJake];
-
-	server.route({
-		method: 'GET',
-		path: '/customers',
-		handler: (request, h) => {
-			return customers;
-		}
-	});
-
-	server.route({
-		method: 'GET',
-		path: '/customers/{customerId}',
-		handler: (request, h) => {
-			const {customerId} = request.params;
-			const customer = customers.find((cust) => cust.customerId === customerId);
-
-			if (!customer) {
-				return h.response().code(404);
-			}
-
-			return customer;
-		}
-	});
-
-	server.route({
-		method: 'POST',
-		path: '/customers',
-		handler: (request, h) => {
-			const customer = request.payload;
-			const existingCustomer = customers.find((cust) => cust.customerId === customer.customerId);
-
-			if (existingCustomer) {
-				return h.response(existingCustomer).code(303);
-			} else {
-				customers.push(customer);
-				return h.response(customer).code(201);
-			}
-		}
-	});
-
-	server.route({
-		method: 'DELETE',
-		path: '/customers/{customerId}',
-		handler: (request, h) => {
-			const {customerId} = request.params;
-			const customer = customers.find((cust) => cust.customerId === customerId);
-
-			if (!customer) {
-				return h.response().code(404);
-			}
-
-			let newCustomers = [];
-
-			customers.forEach((cust) => {
-				if (cust.customerId !== customerId) {
-					newCustomers.push(cust);
-				}
-			});
-
-			customers = newCustomers;
-
-			return '';
-		}
-	});
-
-	server.route({
-		method: 'PUT',
-		path: '/customers/{customerId}',
-		handler: (request, h) => {
-			const {customerId} = request.params;
-			const updatedCustomer = request.payload;
-
-			let newCustomers = [];
-
-			customers.forEach((cust) => {
-				if (cust.customerId === customerId) {
-					newCustomers.push(updatedCustomer);
-				} else {
-					newCustomers.push(cust);
-				}
-			});
-
-			customers = newCustomers;
-
-			return '';
-		}
-	});
-
-await server.start();
-console.log('Server running on %s', server.info.uri);
-
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
 };
 
-process.on("unhandledRejection", (err) => {
-	console.log(err);
-	process.exit(1);
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
 });
+
+init();
